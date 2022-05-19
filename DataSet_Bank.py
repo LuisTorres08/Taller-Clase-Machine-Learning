44c78653456c451e751fecda8fa3aae3b232c3bb
Luis Torres <luisdeavila010@gmail.com>
2022-05-19 00:29:56 -0500

    Obtención de la data

---
+++ DataSet_Bank.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 00:25:39 2022
+
+@author: Luis Torres
+"""
+
+import numpy as np
+import pandas as pd
+
+# Obtener la data
+
+url = "bank-full.csv"
+data = pd.read_csv(url)
+
+
+
